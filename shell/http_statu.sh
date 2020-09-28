31c6c9a3cb6701fc884d1ab239632b71f653a764
叶俊海 <450433231@qq.com>
2020-09-28 18:54:09 +0800

    更新状态检测脚本

--- shell/http_statu.sh
+++ shell/http_statu.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 set -xu
+public_ip=$(curl -s cip.cc|awk '/IP/{printf$3}')
 for ((i=0;i<20;i++))
 do
-	status_code=`curl -s -w "%{http_code}" -m 5 $(curl -s cip.cc|awk '/IP/{print$3}')/ping -o /dev/null`
+	status_code=`curl -s -w "%{http_code}" -m 5 $public_ip/ping -o /dev/null`
 	if [ $status_code -eq "200" ];then
 		exit 0
 	else
